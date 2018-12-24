861c168bb3c68bb3311fc198c064de397c93c6f5
Nathan Pabst <nathan.pabst@gmail.com>
2018-12-24 13:41:02 -0600

    removed unnecessary code and folders.

--- RomanNumeralConversions/Program.cs
+++ RomanNumeralConversions/Program.cs
@@ -9,24 +9,7 @@ namespace RomanNumeralConversions
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Please enter a number between 1 and 3999");
-            //var userInput = int.Parse(Console.ReadLine());
-            //var romanOutput = "";
-            //var evalInput = userInput.ToString().Count();
-            //if (evalInput == 1)
-            //{
-            //    var singleDigits = new SingleDigits();
-            //    romanOutput = SingleDigits.ConvertToRoman(userInput);
-            //}
-            //else if (evalInput == 2)
-            //{
-            //    var dosDigits = new DosDigits();
-            //    romanOutput = DosDigits.ConvertToRoman(userInput);
-            //}
-
-            //Console.WriteLine(romanOutput);
             
-            Console.Read();
         }
     }
 }
